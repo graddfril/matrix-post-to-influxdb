c19d1bedb6af2fe663697965831e19e35e3b2575
Dmitriy Volkov <wldhx@wldhx.me>
2016-09-08 14:58:32 +0300

    feat(influxdb): Post events to InfluxDB as soon as they arrive
    Also:
    - Add a dep on influxdb client
    - Refactor things appropriately

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
+from collections import defaultdict
 import defopt
 from matrix_client.client import MatrixClient, Room
+from influxdb import InfluxDBClient
 
 
-def main(access_token: str, room_id: str, hs: str='https://matrix.org'):
+def transform_matrix_to_influxdb(matrix_event: dict):
+    result = defaultdict(dict)
+
+    result['tags']['sender'] = matrix_event['sender']
+
+    m_e_content = matrix_event['content']
+
+    m_e_content.pop('body') # don't care about body
+    result['measurement'] = m_e_content.pop('msgtype')
+    result['time'] = int(m_e_content.pop('timestamp'))
+    result['fields'] = m_e_content # all things left
+
+    return result
+
+
+def main(access_token: str, room_id: str, influx_dsn: str, hs: str='https://matrix.org'):
     """Listen for events happening in a Matrix room."""
-    client = MatrixClient(hs, token=access_token)
+    matrix = MatrixClient(hs, token=access_token)
+    influxdb = InfluxDBClient.from_DSN(influx_dsn)
 
-    my_room = list(filter(lambda x: x[0] == room_id, client.get_rooms().items()))[0][1]
+    my_room = list(filter(lambda x: x[0] == room_id, matrix.get_rooms().items()))[0][1]
+
     my_room.add_listener(lambda x: print(x))
+    my_room.add_listener(lambda x: influxdb.write_points([transform_matrix_to_influxdb(x)], time_precision='ms'))
 
-    client.listen_forever()
+    matrix.listen_forever()
 
 
 if __name__ == '__main__':
